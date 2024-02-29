20daaab637646d4c02924e1443c8b5d652ae7341
192211500 <123438408+harsha123io@users.noreply.github.com>
2024-02-29 19:21:03 +0530

    Create parentheses.py

---
+++ parentheses.py
@@ -0,0 +1,14 @@
+def generateParenthesis(n):
+    def backtrack(s='', left=0, right=0):
+        if len(s) == 2 * n:
+            result.append(s)
+            return
+        if left < n:
+            backtrack(s + '(', left + 1, right)
+        if right < left:
+            backtrack(s + ')', left, right + 1)
+    result = []
+    backtrack()
+    return result
+n = 3
+print(generateParenthesis(n))
