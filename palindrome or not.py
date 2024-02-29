e828e2b63599c4b5f200b32b23317a51adbf156b
192211500 <123438408+harsha123io@users.noreply.github.com>
2024-02-29 19:12:27 +0530

    Add files via upload

---
+++ palindrome or not.py
@@ -0,0 +1,16 @@
+def is_palindrome(num):
+    original_num = num
+    reversed_num = 0
+
+    while num > 0:
+        digit = num % 10
+        reversed_num = reversed_num * 10 + digit
+        num //= 10
+
+    return original_num == reversed_num
+
+number = int(input("Enter a number: "))
+if is_palindrome(number):
+    print(f"{number} is a palindrome.")
+else:
+    print(f"{number} is not a palindrome.")
